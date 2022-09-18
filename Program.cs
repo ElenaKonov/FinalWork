c0acf138773684a2af1327cf177d68fec7fd43ad
ElenaKonov <nega-le@yandex.ru>
2022-09-18 15:39:09 +0500

    процедура изменена

--- Program.cs
+++ Program.cs
@@ -1,42 +1,27 @@
 ﻿string[] GetArray(int size)
 {
-   string[] result = new string [size];
-    for (int i=0; i< size; i++){
-        Console.WriteLine($"Введите  {i+1}-ый элемент ");
-        result[i]= Console.ReadLine();
-    }
-  return  result; 
+string[] result = new string [size];
+for (int i=0; i< size; i++) {
+Console.WriteLine($"Введите {i+1}-ый элемент ");
+result[i]= Console.ReadLine();
+}
+return result; 
 }
 
 Console.WriteLine("Введите размер массива");
+
 int size=Convert.ToInt32(Console.ReadLine());
 string[] array=GetArray(size);
 Console.WriteLine($"Исходный массив: [{String.Join(" , " , array)}]"); 
- 
- 
-int count=0;
-for (int i=0; i< size; i++){
-        if (array[i].Length< 4){
-        count=count+1;
-        }
-    }
-int sizeNew=count;
-  
-
-string[] GetNewArray(int sizeNew){
-string[] resultNew= new string[sizeNew];
+string[] arrayNew = new string[size];
 int j=0;
-    for (int i=0; i< size; i++){
-     if (array[i].Length< 4){
-        resultNew[j]= array[i];
-        j++;
-    }
-    } 
-  return  resultNew; 
- }
+for (int i=0; i<size; i++) {
+if (array[i].Length< 4) {
+arrayNew[j] = array[i];
+j++;
+}
+} 
+string[] arrayPrint = new string[j];
+Array.Copy(arrayNew, 0, arrayPrint, 0, j);
 
- 
-
-string[] arrayNew=GetNewArray(sizeNew);
-Console.WriteLine($"Результат: [{String.Join(" , " , arrayNew)}]");  
- 
+Console.WriteLine($"Результат: [{String.Join(" , " , arrayPrint)}]"); 
