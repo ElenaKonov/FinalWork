e0dbadce0118fff079487089c9d7e8f47d42651f
ElenaKonov <nega-le@yandex.ru>
2022-09-18 16:38:54 +0500

    добавлены изменения в оформление

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿string[] GetArray(int size)
 {
 string[] result = new string [size];
-for (int i=0; i< size; i++) {
-Console.WriteLine($"Введите {i+1}-ый элемент ");
-result[i]= Console.ReadLine();
-}
+    for (int i=0; i< size; i++) {
+        Console.WriteLine($"Введите {i+1}-ый элемент ");
+        result[i]= Console.ReadLine();
+    }
 return result; 
 }
 
@@ -15,11 +15,11 @@ string[] array=GetArray(size);
 Console.WriteLine($"Исходный массив: [{String.Join(" , " , array)}]"); 
 string[] arrayNew = new string[size];
 int j=0;
-for (int i=0; i<size; i++) {
-if (array[i].Length< 4) {
-arrayNew[j] = array[i];
-j++;
-}
+    for (int i=0; i<size; i++) {
+       if (array[i].Length< 4) {
+       arrayNew[j] = array[i];
+        j++;
+    }
 } 
 string[] arrayPrint = new string[j];
 Array.Copy(arrayNew, 0, arrayPrint, 0, j);
